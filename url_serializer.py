3f1924f4f6e0098b9a2b88f1c55be812efa96dfa
g-lajoie <glajoie00@gmail.com>
2024-12-26 21:39:13 +0000

    Updated url_seralizer.py

--- url_serializer.py
+++ url_serializer.py
@@ -1,11 +1,38 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from urllib.parse import urlparse
 
 class ISerializer(ABC):
 
-    def url_information():
+    @abstractmethod
+    def url_information(self):
         "Return URL Information"
 
+    @abstractmethod
+    @property
+    def url(self):
+        "url attribute"
+    
 class UrlSerializer:
 
-    def ___init__(self):
-        pass
+    def ___init__(self, url):
+        self.url = url
+
+    @property
+    def url(self):
+        return self.url
+    
+    def parse_url(self):
+        parse_result = urlparse(self.url)
+        
+        return {
+            "scheme": parse_result.scheme,
+            "netloc": parse_result.netloc,
+            "path": parse_result.path,
+            "hostname": parse_result.hostname
+        }
+
+    @property
+    def hostname(self):
+        return self.parse_url(self.url).hostname
+
+    
