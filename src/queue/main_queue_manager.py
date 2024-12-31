26a8f7199cdd0e3eb932f41a9daa3d14361dd3e4
g-lajoie <glajoie00@gmail.com>
2024-12-31 15:33:24 +0000

    Updated main queue manager

--- src/queue/main_queue_manager.py
+++ src/queue/main_queue_manager.py
@@ -1,28 +1,5 @@
-# Imports
-from src.serializer import Deserializer
-from typing import Dict, Any, List
 
-# End Imports
 
-# Type Aliases
-JSON = Dict[str, Any]
-
-# Implementation
-class MainQueueManager():
-    def __init__(self, deserializer:Deserializer):
-        self.websites:List = []
-        self.deserializer = deserializer
-            
-    def get_websites(self):       
-        return self.deserializer.create_website_list()
-    
-    def insert_into_websites(self, type:str, path:str = None):
-        # Enforce format for type parameter.
-        type = type.lower()
-        
-        # Add websites to websites list
-        if type == 'json' and path is not None:
-            self.websites.extend(JsonWebsiteConverter(path).create_website_list())
     
     
         
